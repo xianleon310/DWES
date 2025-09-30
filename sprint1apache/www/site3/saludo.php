fc0dea5ca271b315f9fd3138a7a6b19fd2af3c83
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-09-30 13:02:37 +0200

    Sprint1-p2: añadido sitio con php

---
+++ sprint1apache/www/site3/saludo.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombre = "Xián";
+
+// Obtenemos el día de la semana en inglés usando date("l")
+$diaSemana = date("l");
+
+// Mostramos el mensaje
+echo "Hola, soy $nombre y hoy es $diaSemana";
+?>
