845c8d65537c4adfeb1162603d10079b316e2f60
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-10-15 18:18:56 +0200

    Completado ejercicio 2

--- sprint2phpsql/www/site4/main.php
+++ sprint2phpsql/www/site4/main.php
@@ -7,20 +7,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Base de datos</title>
+    <link rel="stylesheet" href="main-estilos.css">
 </head>
 <body>
-    <h1>Conexion establecida</h1>
+    <h1>Conexión establecida</h1>
     <?php
+        if ($_GET["id"]){
+            $id=$_GET["id"];
+        }
         //Lanzar consulta
         $consulta='SELECT * FROM tJuegos';
         $resultado=mysqli_query($db,$consulta) or die ('Error de consulta');
         while ($row=mysqli_fetch_array($resultado)){
-            echo $row['nombre'];
-            echo "<br>";
-            echo "<img width=100 height=100 src='";
-            echo $row[2];
-            echo "'></img>";
-            echo "<br>";
+            if ($row['id']==$id){
+                echo "<h2>";
+                echo $row['nombre'];
+                echo "</h2>";
+                echo "<img width=100 height=100 src='";
+                echo $row[2];
+                echo "'></img>";
+                echo "<p>";
+                echo $row['descripcion'];
+                echo "</p>";
+                echo $row['plataforma'];
+            }
         }    
     ?>
 </body>
