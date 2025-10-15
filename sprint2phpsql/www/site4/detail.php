5b19aa4a6c5bec8d3ff9f656b68ee5f39d1bee96
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-10-15 19:41:55 +0200

    Correcciones varias

--- sprint2phpsql/www/site4/detail.php
+++ sprint2phpsql/www/site4/detail.php
@@ -19,34 +19,29 @@
         }
 
         //Lanzar consultas
-        $consultatJuegos='SELECT * FROM tJuegos';
+        $consultatJuegos='SELECT * FROM tJuegos WHERE id='.$id;
         $resultadotJuegos=mysqli_query($db,$consultatJuegos) or die ('Error de consulta');
-
+        $row=mysqli_fetch_array($resultadotJuegos);
         $consultatComentarios = "SELECT comentario FROM tComentarios WHERE juego_id = $id";
         $resultadotComentarios = mysqli_query($db, $consultatComentarios) or die("Error al consultar comentarios");
 
-        //Bucle recorriendo todas las filas y plasmandolo
-        while ($row=mysqli_fetch_array($resultadotJuegos)){
-            if ($row['id']==$id){
-                echo "<h2>";
-                echo $row['nombre'];
-                echo "</h2>";
-                echo "<img width=100 height=100 src='";
-                echo $row[2];
-                echo "'></img>";
-                echo "<p><em>";
-                echo $row['descripcion'];
-                echo "</em></p>";
-                echo $row['plataforma'];
-                echo "<p class='rojo'>Comentarios:</p>";
-                //Mostrar todos los comentarios relacionados con ese id de juego
-                echo "<ul>";
-                while ($com = mysqli_fetch_array($resultadotComentarios)) {
-                    echo "<li>" . $com['comentario'] . "</li>";
-                }
-                echo "</ul>";
-            }
-        }    
+        echo "<h2>";
+        echo $row['nombre'];
+        echo "</h2>";
+        echo "<img width=100 height=100 src='";
+        echo $row[2];
+        echo "'></img>";
+        echo "<p><em>";
+        echo $row['descripcion'];
+        echo "</em></p>";
+        echo $row['plataforma'];
+        echo "<p class='rojo'>Comentarios:</p>";
+        //Mostrar todos los comentarios relacionados con ese id de juego
+        echo "<ul>";
+        while ($com = mysqli_fetch_array($resultadotComentarios)) {
+            echo "<li>" . $com['comentario'] . "</li>";
+        }
+        echo "</ul>";   
     ?>
 </body>
 </html>
