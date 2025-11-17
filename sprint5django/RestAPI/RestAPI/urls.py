3a144d79371ffb70665952c488cbbe42ecf06be6
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-11-17 09:15:37 +0100

    completado primer get a api

--- sprint5django/RestAPI/RestAPI/urls.py
+++ sprint5django/RestAPI/RestAPI/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webserviceapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test',views.pagina_de_prueba),
+    path('juegos',views.devolver_canciones)
 ]
--- sprint5django/RestAPI/webserviceapp/views.py
+++ sprint5django/RestAPI/webserviceapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,JsonResponse
+from .models import Tjuegos
 # Create your views here.
+def pagina_de_prueba(request):
+    return HttpResponse("<h1>Hola caracola</h1>")
+
+def devolver_canciones(request):
+    lista= Tjuegos.objects.all()
+    respuesta_final=[]
+    for fila_sql in lista:
+        diccionario={}
+        diccionario['id']=fila_sql.id
+        diccionario['nombre']=fila_sql.nombre
+        diccionario['descripcion']=fila_sql.descripcion
+        diccionario['plataforma']=fila_sql.plataforma
+        respuesta_final.append(diccionario)
+    return JsonResponse(respuesta_final,safe=False)
