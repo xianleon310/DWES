585f145a70babeb0cb789540c651f9616fb23252
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2026-01-21 12:51:31 +0100

    Añadido filter

--- sprint6Juegos/config/settings.py
+++ sprint6Juegos/config/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     'juegos',
     'rest_framework',
     'corsheaders',
+    'django_filters',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
